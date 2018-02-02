de8e9787473914641443594a25d128f36657ad28
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-02 09:43:22 -0600

    pyschic game debugged

--- assets/javascript/pyschicgame.js
+++ assets/javascript/pyschicgame.js
@@ -1,19 +1,18 @@
+// variables
+////////////////
 
 //determines the computer's choice
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//variables for win, loss, guess
+    //variables for win, loss, guess
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-var userLetters = []
+var userLetters = [];
 
-function addLetters(userChoice) {
-    userLetters.push(userChoice);
-    console.log("Your Guesses so far: " + userLetters.join(", "));
-}
-;
+//Random computer choice
+    var computerChoice;
 
 //use to grab elements in our markup
 
@@ -22,15 +21,37 @@ var userLossesDiv = document.getElementById("losses");
 var userGuessLeftDiv = document.getElementById("guesses");
 var userLettersDiv = document.getElementById("userLetters");
 
-//function for when a key is pushed
+// functions
+//////////////////
+
+function addLetters(userChoice) {
+    userLetters.push(userChoice);
+    console.log("Your Guesses so far: " + userLetters.join(", "));
+}
+;
+
+//determine default setting for when game is load, 
+//computer makes choice; wins, losses, and chosen 
+//letters are blank, guess remain in 9
+
+function start () {
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice)
+}
+
+
+
+// main logic
+//////////////////
+
+start()
+
+  //function for when a key is pushed
 document.onkeyup = function (event) {
+   
 
     //determines letter picked
     var userChoice = event.key;
-
-    //Random computer choice
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(computerChoice)
 
     //lists the letters the user has 
 
@@ -38,9 +59,14 @@ document.onkeyup = function (event) {
 
     //Logic to determine outcomes and score
 
+
     if (userChoice === computerChoice) {
-        wins++ && guesses--;
-    } else if (userChoice != computerChoice) {
+        wins++;
+        guesses = 9
+        userLetters = []
+        alert("You won!");
+        start()
+    } else {
         guesses--;
         addLetters(userChoice)
     }
@@ -49,7 +75,13 @@ document.onkeyup = function (event) {
         losses++;
         guesses = 9
         userLetters = []
+        alert("You lose! Try again.")
+        start()
     }
+
+    
+
+
 
     //update the scoreboard
     userWinsDiv.textContent = wins;
@@ -57,3 +89,5 @@ document.onkeyup = function (event) {
     userGuessLeftDiv.textContent = guesses;
     userLettersDiv.textContent = userLetters.join(", ");
 }
+
+
